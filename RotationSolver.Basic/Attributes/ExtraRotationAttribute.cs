f90a3a78eecd6b4571212b098b1b4fe5fe8e2462
YangYangaaaa <YangYangaaaa@users.noreply.github.com>
2026-07-05 04:32:02 +0000

    Deploy pluginmaster.json for 2026.7.5.0431

--- RotationSolver.Basic/Attributes/ExtraRotationAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RotationSolver.Basic.Attributes;
-
-/// <summary>
-/// Tag for rotations that are 3rd party (Extra).
-/// </summary>
-[AttributeUsage(AttributeTargets.Class)]
-public class ExtraRotationAttribute : Attribute
-{
-}
--- RotationSolver.Basic/Attributes/IDAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RotationSolver.Basic.Attributes;
-
-[AttributeUsage(AttributeTargets.Property)]
-internal class IDAttribute(uint id) : Attribute
-{
-	public uint ID => id;
-}
